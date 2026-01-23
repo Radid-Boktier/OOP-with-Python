b374b3d328cb8cffc8b3e87affa2dbc20e293971
Radid Ahammed <radidboktier823@gmail.com>
2026-01-23 22:45:53 +0600

    Implement Item.all to store all instances and add __repr__ for readable output

--- 03 - Class Attributes/main.py
+++ 03 - Class Attributes/main.py
@@ -1,6 +1,7 @@
 #Create a Class
 class Item:
   pay_rate = 0.8
+  all = []
   def __init__(self,name,price,quantity=0):
     #Run validations to the received arguments
     assert price >= 0, f"Price {price} is not greater than or equal to zero"
@@ -11,31 +12,28 @@ class Item:
     self.price = price
     self.quantity = quantity
 
+    # Actions to execute
+    Item.all.append(self)
+
   def calculate_total_price(self):
     return self.price*self.quantity
   
   def apply_discount(self):
     self.price = self.price * self.pay_rate
+
+  def __repr__(self):
+    return f"Item({self.name}, {self.price}, {self.quantity})"
   
-#create instance of a class
-item1 = Item('Phone',100,3)
-item2 = Item('Laptop',1000,2)
-
-print(Item.pay_rate)
-#First check instance level if not find then go class level for attributes
-print(item1.pay_rate)
-print(item2.pay_rate)
-
-print(Item.__dict__) #All the attributes for class level 
-print(item1.__dict__) #All the attributes for instance level
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
 
-item1.apply_discount()
-print(item1.price)
+for instance in Item.all:
+  print(instance.name)
 
-item2.pay_rate = 0.7;
-item2.apply_discount();
-print(item2.price)
-
+print(Item.all)
 
 
