f8cd6c340a5cd832a662c3288968db0da039e4e5
Radid Ahammed Boktier <radidboktier@gmail.com>
2026-02-01 16:07:48 +0600

    Apply abstraction to email sending logic in Item class

---
+++ 07 - OOP Principles/item.py
@@ -0,0 +1,100 @@
+import csv
+import os
+
+base_dir = os.path.dirname(__file__)  # folder containing main.py
+csv_path = os.path.join(base_dir, 'items.csv')
+
+#Create a Class
+class Item:
+  pay_rate = 0.8
+  all = []
+  def __init__(self,name,price,quantity=0):
+    #Run validations to the received arguments
+    assert price >= 0, f"Price {price} is not greater than or equal to zero"
+    assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero"
+
+    #Assign to self object
+    self.__name = name
+    self.__price = price
+    self.quantity = quantity
+
+    # Actions to execute
+    Item.all.append(self)
+
+  @property
+  def price(self):
+    return self.__price
+   
+  def apply_discount(self):
+    self.__price = self.__price * self.pay_rate
+
+  def apply_increment(self,increment_value):
+    self.__price = self.__price + self.__price * increment_value
+
+  #Getter Function
+  @property
+  # Property decorator = read only Attribute
+  def name(self):
+    print("get name")
+    return self.__name
+  
+  #Setter function
+  @name.setter
+  def name(self,value):
+    print("Set name")
+
+    if len(value) > 10:
+      raise Exception("The name is too long!")
+    else:
+      self.__name = value
+
+
+  def calculate_total_price(self):
+    return self.__price*self.quantity
+  
+
+
+  @classmethod
+  def instantiate_from_csv(cls):
+    with open(csv_path,'r') as f:
+      reader = csv.DictReader(f)
+      items = list(reader)
+
+    for item in items:
+      # Item(item.get('name'),float(item.get('price')),int(item.get('quantity')))
+      Item(
+        name = item.get('name'),
+        price = float(item.get('price')),
+        quantity = int(item.get('quantity'))
+      )
+
+  @staticmethod
+  def is_integer(num):
+    # We will count out the floats that are point zero
+    # For i.e: 5.0, 10.0
+    if isinstance(num,float):
+      # Count out the floats that are point zero
+      return num.is_integer()
+    elif isinstance(num,int):
+      return True
+    else:
+      return False
+
+  # def __repr__(self):
+  #   return f"{self.__class__.__name__}({self.name}, {self.price}, {self.quantity})"
+  def __connect(self,smpt_server):
+    pass
+  
+  def __prepare_body(self):
+    return f"""
+    Hello Someone.
+    We have {self.name} {self.quantity} times.
+    Regards, Boktier
+    """
+  def __send(self):
+    pass
+  
+  def send_email(self):
+    self.__connect('')
+    self.__prepare_body()
+    self.__send()
